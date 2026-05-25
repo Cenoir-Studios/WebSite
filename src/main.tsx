32d38963ca7e8fb264aa6639d41f6a2a746ff26c
Daniil <75696359+DaniilDenysov@users.noreply.github.com>
2026-05-25 22:49:00 +0200

    Revert "Update"
    This reverts commit bd81f98e7a8b7b3e2b7469d1398a7867744cf26a.

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { loadAllContent } from './content/loader';
-import App from './App';
-import './style.css';
-
-loadAllContent().then(() => {
-  createRoot(document.getElementById('root')!).render(<App />);
-});
